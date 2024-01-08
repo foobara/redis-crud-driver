b5eae56665409aed39688b6f59d7858c3922da25
Miles Georgi <azimux@gmail.com>
2024-01-08 23:45:22 +0000

    Initial commit, only .gitignore changed, created from RubyMine

---
+++ lib/redis_crud_driver.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "redis_crud_driver/version"
+
+module RedisCrudDriver
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/redis_crud_driver/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RedisCrudDriver
+  VERSION = "0.1.0"
+end
