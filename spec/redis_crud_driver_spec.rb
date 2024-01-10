3ee854f6b5a04742a3c106fec26ac39bdf6be3ce
Miles Georgi <azimux@gmail.com>
2024-01-09 18:50:43 -0800

    Implement an initial RedisCrudDriver

--- spec/redis_crud_driver_spec.rb
+++ spec/redis_crud_driver_spec.rb
@@ -1,5 +1,612 @@
 RSpec.describe Foobara::RedisCrudDriver do
+  let(:entity_class) do
+    stub_class("SomeEntity", Foobara::Entity) do
+      attributes pk: :integer,
+                 foo: :integer,
+                 bar: :symbol
+
+      primary_key :pk
+    end
+  end
+  let(:credentials) { test_redis }
+  let(:test_credentials) { "redis://localhost:6379/3" }
+  let(:test_redis) { Redis.new(url: test_credentials) }
+
+  after do
+    Foobara.reset_alls
+    # TODO: make this a project and put this in reset_all
+    if described_class.instance_variable_defined?(:@redis)
+      described_class.remove_instance_variable(:@redis)
+    end
+  end
+
+  before do
+    test_redis.flushdb
+    Foobara::Persistence.default_crud_driver = described_class.new(credentials)
+  end
+
   it "has a version number" do
-    expect(Foobara::RedisCrudDriver::Version::VERSION).to_not be_nil
+    expect(Foobara::RedisCrudDriverVersion::VERSION).to_not be_nil
+  end
+
+  describe ".redis" do
+    def creds(redis)
+      client = redis._client
+
+      {
+        host: client.host,
+        port: client.port,
+        db: client.db,
+        username: client.username
+      }
+    end
+
+    context "with no REDIS_URL env var" do
+      stub_env_var("REDIS_URL", nil)
+
+      it "defaults to Redis.new" do
+        expect(creds(described_class.redis)).to eq(creds(Redis.new))
+      end
+    end
+
+    context "with REDIS_URL env var" do
+      stub_env_var("REDIS_URL", "redis://localhost:6379/1")
+
+      it "defaults to Redis.new" do
+        expect(creds(described_class.redis)).to eq(
+          host: "localhost",
+          port: 6379,
+          db: 1,
+          username: "default"
+        )
+      end
+    end
+  end
+
+  describe "#initialize" do
+    let(:driver) { described_class.new(credentials) }
+
+    let(:fake_redis) { Redis.new }
+    let(:fake_client) { fake_redis._client }
+
+    let(:username) { fake_client.username }
+    let(:password) { fake_client.password }
+    let(:host) { fake_client.host }
+    let(:port) { fake_client.port }
+    let(:db) { fake_client.db + 1 }
+
+    context "when using nothing" do
+      let(:credentials) { nil }
+
+      it "uses RedisCrudDriver.redis" do
+        expect(driver.raw_connection).to eq(described_class.redis)
+      end
+    end
+
+    context "when using Redis instance" do
+      let(:credentials) { fake_redis }
+
+      it "uses the existing connection" do
+        expect(driver.raw_connection).to be(credentials)
+      end
+    end
+
+    context "when using url" do
+      let(:credentials) do
+        "redis://#{username}:#{password}@#{host}:#{port}/#{db}"
+      end
+
+      it "connects using the url" do
+        expect(driver.raw_connection._client.db).to eq(db)
+      end
+    end
+
+    context "when using hash" do
+      let(:credentials) do
+        {
+          host:,
+          port:,
+          db:,
+          username:,
+          password:
+        }
+      end
+
+      it "connects using the hash" do
+        expect(driver.raw_connection._client.db).to eq(db)
+      end
+    end
+  end
+
+  describe ".transaction" do
+    it "can create, load, and update records" do
+      expect {
+        entity_class.create(foo: 1, bar: :baz)
+      }.to raise_error(Foobara::Persistence::EntityBase::Transaction::NoCurrentTransactionError)
+
+      transaction = nil
+
+      entity1 = entity_class.transaction do |tx|
+        transaction = tx
+
+        entity = entity_class.create(foo: 1, bar: :baz)
+
+        expect(entity).to be_a(entity_class)
+        expect(entity).to_not be_persisted
+        expect(entity).to_not be_loaded
+
+        expect(tx).to be_open
+        expect(Foobara::Persistence.current_transaction(entity)).to be(tx)
+
+        entity
+      end
+
+      expect(transaction).to be_closed
+      expect(Foobara::Persistence.current_transaction(entity1)).to be_nil
+
+      expect(entity1).to be_a(entity_class)
+      expect(entity1).to be_persisted
+      expect(entity1).to be_loaded
+
+      entity_class.transaction do
+        entity = entity_class.thunk(entity1.primary_key)
+
+        expect(entity).to be_a(entity_class)
+        expect(entity).to be_persisted
+        expect(entity).to_not be_loaded
+
+        expect(entity.bar).to eq(:baz)
+
+        expect(entity).to be_loaded
+
+        singleton = entity_class.thunk(entity.primary_key)
+        expect(singleton).to be(entity)
+
+        entity.bar = "bazbaz"
+      end
+
+      entity_class.transaction do
+        entity = Foobara::Persistence.current_transaction(entity_class).load(entity_class, entity1.primary_key)
+        expect(entity.bar).to eq(:bazbaz)
+
+        expect(entity_class.all.to_a).to eq([entity1])
+      end
+    end
+
+    it "can rollback" do
+      entity1 = entity_class.transaction do
+        entity_class.create(foo: 10, bar: :baz)
+      end
+
+      entity_class.transaction do |tx|
+        entity = entity_class.thunk(entity1.primary_key)
+        expect(entity.foo).to eq(10)
+
+        entity.foo = 20
+
+        expect(entity.foo).to eq(20)
+
+        begin
+          tx.rollback!
+        rescue Foobara::Persistence::EntityBase::Transaction::RolledBack # rubocop:disable Lint/SuppressedException
+        end
+
+        expect(entity.foo).to eq(10)
+
+        entity_class.transaction do
+          expect(entity_class.load(entity.primary_key).foo).to eq(10)
+        end
+
+        expect {
+          entity.foo = 20
+        }.to raise_error(Foobara::Persistence::EntityBase::Transaction::NoCurrentTransactionError)
+      end
+
+      entity_class.transaction do |tx|
+        entity = entity_class.load(entity1.primary_key)
+        entity = entity_class.load(entity.primary_key)
+        expect(entity.foo).to eq(10)
+
+        entity.foo = 20
+
+        expect(entity.foo).to eq(20)
+
+        tx.flush!
+
+        expect(entity.foo).to eq(20)
+
+        entity.foo = 30
+
+        tx.revert!
+
+        expect(entity.foo).to eq(20)
+      end
+
+      entity_class.transaction do
+        entity = entity_class.load(entity1.primary_key)
+        expect(entity.foo).to eq(20)
+      end
+    end
+
+    it "can hard delete" do
+      entity_class.transaction do
+        expect(entity_class.all.to_a).to be_empty
+        entity = entity_class.create(foo: 10, bar: :baz)
+        expect(entity_class.all.to_a).to eq([entity])
+        entity.hard_delete!
+        expect(entity_class.all.to_a).to be_empty
+      end
+
+      entity1 = entity_class.transaction do
+        expect(entity_class.all.to_a).to be_empty
+        entity_class.create(foo: 10, bar: :baz)
+      end
+
+      entity_class.transaction do
+        entity = entity_class.thunk(entity1.primary_key)
+        expect(entity.foo).to eq(10)
+
+        entity.hard_delete!
+
+        expect(entity).to be_hard_deleted
+
+        # TODO: make this work without needing to call #to_a
+        expect(entity_class.all.to_a).to be_empty
+
+        expect {
+          entity.foo = 20
+        }.to raise_error(Foobara::Entity::CannotUpdateHardDeletedRecordError)
+
+        expect(entity.foo).to eq(10)
+
+        entity.restore!
+        expect(entity_class.all.to_a).to eq([entity])
+
+        entity.foo = 20
+      end
+
+      entity_class.transaction do
+        # TODO: make calling #to_a not necessary
+        expect(entity_class.all.to_a).to eq([entity1])
+        entity = entity_class.thunk(entity1.primary_key)
+
+        expect(entity).to be_persisted
+        expect(entity).to_not be_hard_deleted
+        expect(entity.foo).to eq(20)
+
+        entity.hard_delete!
+
+        expect(entity_class.all.to_a).to be_empty
+        expect(entity).to be_hard_deleted
+      end
+
+      entity_class.transaction do
+        expect {
+          entity_class.load(entity1.primary_key)
+          # TODO: come up with a more sensible error
+        }.to raise_error(Foobara::Persistence::EntityAttributesCrudDriver::Table::CannotFindError)
+
+        expect(entity_class.all.to_a).to be_empty
+      end
+    end
+
+    describe "#hard_delete_all" do
+      it "deletes everything" do
+        entities = []
+
+        entity_class.transaction do
+          4.times do
+            entity = entity_class.create(foo: 1, bar: :baz)
+            entities << entity
+          end
+
+          # TODO: make calling #to_a not necessary
+          expect(entity_class.all.to_a).to eq(entities)
+        end
+
+        entity_ids = entities.map(&:primary_key)
+
+        expect(entity_ids).to contain_exactly(1, 2, 3, 4)
+
+        entity_class.transaction do
+          entities = []
+
+          entity_class.all do |record|
+            entities << record
+          end
+
+          entity_ids = entities.map(&:primary_key)
+
+          expect(entity_ids).to contain_exactly(1, 2, 3, 4)
+
+          4.times do
+            entity = entity_class.create(foo: 1, bar: :baz)
+            entities << entity
+          end
+
+          expect(entity_class.all).to match_array(entities)
+
+          Foobara::Persistence.current_transaction(entities.first).hard_delete_all!(entity_class)
+
+          expect(entities).to all be_hard_deleted
+          expect(entity_class.all.to_a).to be_empty
+        end
+
+        entity_class.transaction do
+          expect(entity_class.all.to_a).to be_empty
+        end
+      end
+    end
+
+    describe "#truncate" do
+      it "deletes everything" do
+        entity_class.transaction do
+          4.times do
+            entity_class.create(foo: 1, bar: :baz)
+          end
+
+          # TODO: make calling #to_a not necessary
+          expect(entity_class.count).to eq(4)
+        end
+
+        entity_class.transaction do
+          expect(entity_class.count).to eq(4)
+
+          Foobara::Persistence.current_transaction(entity_class).truncate!
+
+          expect(entity_class.count).to eq(0)
+          expect(entity_class.all.to_a).to be_empty
+        end
+
+        entity_class.transaction do
+          expect(entity_class.count).to eq(0)
+          expect(entity_class.all.to_a).to be_empty
+        end
+      end
+    end
+
+    describe "#load_many" do
+      it "loads many" do
+        entities = nil
+        entity_ids = nil
+
+        entity_class.transaction do |tx|
+          [
+            { foo: 11, bar: :baz },
+            { foo: 22, bar: :baz },
+            { foo: 33, bar: :baz },
+            { foo: 44, bar: :baz }
+          ].map do |attributes|
+            entity_class.create(attributes)
+          end
+
+          expect(entity_class.count).to eq(4)
+
+          entity_class.transaction(mode: :use_existing) do
+            expect(entity_class.count).to eq(4)
+          end
+
+          tx2 = entity_class.transaction(mode: :use_existing)
+
+          entity_class.entity_base.using_transaction(tx2) do
+            expect(entity_class.count).to eq(4)
+          end
+
+          entity_class.transaction(mode: :open_nested) do
+            expect(entity_class.count).to eq(0)
+          end
+
+          tx.flush!
+
+          entity_class.transaction(mode: :use_existing) do
+            expect(entity_class.count).to eq(4)
+          end
+
+          entity_class.transaction(mode: :open_nested) do
+            expect(entity_class.count).to eq(4)
+          end
+
+          entities = entity_class.all
+
+          expect(entities).to all be_a(Foobara::Entity)
+          expect(entities.size).to eq(4)
+
+          entity_ids = entities.map(&:primary_key)
+          expect(entity_ids).to contain_exactly(1, 2, 3, 4)
+        end
+
+        entity_class.transaction do
+          entity_class.load_many([entity_class.thunk(1)])
+          loaded_entities = entity_class.load_many(entity_ids)
+          expect(loaded_entities).to all be_loaded
+          expect(loaded_entities).to eq(entities)
+        end
+      end
+    end
+
+    describe "#all_exist?" do
+      it "answers whether they all exist or not" do
+        entity_class.transaction do
+          expect(entity_class.all_exist?([101, 102])).to be(false)
+
+          [
+            { foo: 11, bar: :baz, pk: 101 },
+            { foo: 22, bar: :baz, pk: 102 },
+            { foo: 33, bar: :baz },
+            { foo: 44, bar: :baz }
+          ].map do |attributes|
+            entity_class.create(attributes)
+          end
+
+          entity_class.all do |record|
+            expect(record).to_not be_persisted
+          end
+
+          expect(entity_class.all_exist?([101, 102])).to be(true)
+          expect(entity_class.all_exist?([1, 2, 101, 102])).to be(false)
+        end
+
+        entity_class.transaction do
+          expect(entity_class.all_exist?([1, 2, 101, 102])).to be(true)
+          expect(entity_class.all_exist?([3])).to be(false)
+        end
+      end
+    end
+
+    describe "#unhard_delete!" do
+      context "when record was dirty when hard deleted" do
+        it "is still dirty" do
+          entity = entity_class.transaction do
+            entity_class.create(foo: 11, bar: :baz)
+          end
+
+          entity_class.transaction do
+            entity = entity_class.thunk(entity.primary_key)
+
+            expect(entity).to be_persisted
+
+            expect(entity).to_not be_dirty
+
+            entity.foo = 12
+
+            expect(entity).to be_dirty
+            expect(entity).to_not be_hard_deleted
+
+            entity.foo = 11
+
+            expect(entity).to_not be_dirty
+            expect(entity).to_not be_hard_deleted
+
+            entity.foo = 12
+
+            expect(entity).to be_dirty
+            expect(entity).to_not be_hard_deleted
+
+            entity.hard_delete!
+
+            expect(entity).to be_dirty
+            expect(entity).to be_hard_deleted
+
+            entity.unhard_delete!
+
+            expect(entity).to be_dirty
+            expect(entity).to_not be_hard_deleted
+          end
+        end
+      end
+    end
+
+    describe "#exists?" do
+      it "answers it exists or not" do
+        entity_class.transaction do
+          expect(entity_class.all_exist?([101, 102])).to be(false)
+
+          entity_class.create(foo: 11, bar: :baz, pk: 101)
+
+          expect(entity_class.exists?(101)).to be(true)
+
+          entity_class.create(foo: 11, bar: :baz)
+
+          expect(entity_class.exists?(1)).to be(false)
+        end
+
+        entity_class.transaction do
+          expect(entity_class.exists?(101)).to be(true)
+
+          expect(entity_class.exists?(1)).to be(true)
+          expect(entity_class.exists?(2)).to be(false)
+        end
+      end
+    end
+
+    context "when creating a record with an already-in-use key" do
+      it "explodes" do
+        entity_class.transaction do
+          entity_class.create(foo: 11, bar: :baz, pk: 101)
+        end
+
+        expect {
+          entity_class.transaction do
+            entity_class.create(foo: 11, bar: :baz, pk: 101)
+          end
+        }.to raise_error(Foobara::Persistence::EntityAttributesCrudDriver::Table::CannotInsertError)
+      end
+    end
+
+    context "when restoring with a created record" do
+      it "hard deletes it" do
+        entity_class.transaction do |tx|
+          record = entity_class.create(foo: 11, bar: :baz, pk: 101)
+
+          tx.revert!
+
+          expect(record).to be_hard_deleted
+        end
+
+        entity_class.transaction do
+          expect(entity_class.count).to eq(0)
+        end
+      end
+    end
+
+    context "when persisting entity with an association" do
+      let(:aggregate_class) do
+        stub_class "SomeAggregate", Foobara::Entity do
+          attributes pk: :integer,
+                     foo: :integer,
+                     some_entities: [SomeEntity]
+
+          primary_key :pk
+        end
+      end
+
+      it "writes the records to disk using primary keys" do
+        some_entity2 = nil
+
+        some_entity1 = entity_class.transaction do
+          some_entity2 = entity_class.create(foo: 11, bar: :baz)
+          entity_class.create(foo: 11, bar: :baz, pk: 101)
+        end
+
+        entity_class.transaction do
+          some_entity3 = entity_class.create(foo: 11, bar: :baz, pk: 102)
+          some_entity4 = entity_class.create(foo: 11, bar: :baz)
+
+          aggregate_class.create(
+            foo: 30,
+            some_entities: [
+              1,
+              some_entity1,
+              some_entity3,
+              some_entity4
+            ]
+          )
+        end
+
+        entity_class.transaction do |tx|
+          crud_table = aggregate_class.current_transaction_table.entity_attributes_crud_driver_table
+          raw_records = crud_table.all.to_a
+          expect(raw_records.size).to eq(1)
+          raw_record = raw_records.first
+          expect(raw_record[:some_entities]).to contain_exactly(1, 2, 101, 102)
+
+          loaded_aggregate = aggregate_class.load(1)
+          expect(loaded_aggregate.some_entities).to all be_a(SomeEntity)
+          expect(loaded_aggregate.some_entities.map(&:primary_key)).to contain_exactly(1, 2, 101, 102)
+
+          new_aggregate = aggregate_class.create(
+            foo: 30,
+            some_entities: [
+              entity_class.create(foo: 11, bar: :baz)
+            ]
+          )
+
+          expect(aggregate_class.contains_associations?).to be(true)
+          expect(entity_class.contains_associations?).to be(false)
+
+          tx.flush_created_record!(new_aggregate)
+        end
+      end
+    end
   end
 end
---
+++ src/redis_crud_driver.rb
@@ -0,0 +1,254 @@
+module Foobara
+  class RedisCrudDriver < Persistence::EntityAttributesCrudDriver
+    class << self
+      attr_writer :redis
+
+      def redis
+        @redis ||= if ENV["REDIS_URL"]
+                     Redis.new(url: ENV["REDIS_URL"])
+                   else
+                     Redis.new
+                   end
+      end
+    end
+
+    attr_reader :prefix
+
+    def initialize(connection_or_credentials = nil, prefix: nil)
+      @prefix = prefix
+      super(connection_or_credentials)
+    end
+
+    def open_connection(connection_url_or_credentials)
+      case connection_url_or_credentials
+      when Redis
+        connection_url_or_credentials
+      when ::String
+        Redis.new(url: connection_url_or_credentials)
+      when ::Hash
+        Redis.new(connection_url_or_credentials)
+      when nil
+        self.class.redis
+      end
+    end
+
+    # We don't make use of Redis transaction feature here because they are do not really work
+    # like database transactions which is what this feature is based on. We might want to use database transactions
+    # in some sort of flush feature though.
+    def open_transaction
+      # Should we have some kind of fake transaction object that raises errors when used after rolledback/closed?
+      Object.new
+    end
+
+    def close_transaction(_raw_tx)
+    end
+
+    def rollback_transaction(_raw_tx)
+      # nothing to do... except maybe enter a state where we don't flush anything else
+      # but can just rely on higher-up plumbing for that
+    end
+
+    class Table < Persistence::EntityAttributesCrudDriver::Table
+      def initialize(...)
+        super
+
+        unless entity_class.primary_key_type.type_symbol == :integer
+          # TODO: when primary key is a string such as a uuid we should
+          # probably set the score to 0 and use other methods. But not interested in implementing that stuff now.
+          # :nocov:
+          raise "Only integer primary keys are supported for now"
+          # :nocov:
+        end
+      end
+
+      def prefix
+        crud_driver.prefix
+      end
+
+      def get_id
+        redis.incr(sequence_key)
+      end
+
+      def sequence_key
+        @sequence_key ||= "#{entity_key_prefix}$sequence"
+      end
+
+      def primary_keys_index_key
+        @primary_keys_index_key ||= "#{entity_key_prefix}$all"
+      end
+
+      def entity_key_prefix
+        @entity_key_prefix ||= [*prefix, table_name].join(":")
+      end
+
+      def record_key_prefix(record_id)
+        "#{entity_key_prefix}:#{record_id}"
+      end
+
+      def all
+        Enumerator.new do |yielder|
+          batches_of_primary_keys.each do |batch|
+            raw_records = redis.pipelined do |p|
+              batch.each do |record_id|
+                p.hgetall(record_key_prefix(record_id))
+              end
+            end
+
+            raw_records.each do |raw_record|
+              yielder << restore_attributes_from_redis(raw_record)
+            end
+          end
+        end.lazy
+      end
+
+      def count
+        redis.zcount(primary_keys_index_key, "-inf", "+inf")
+      end
+
+      def find(record_id)
+        pairs = redis.hgetall(record_key_prefix(record_id))
+
+        unless pairs.empty?
+          restore_attributes_from_redis(pairs.to_h)
+        end
+      end
+
+      def find!(record_id)
+        attributes = find(record_id)
+
+        unless attributes
+          raise CannotFindError.new(record_id, "does not exist")
+        end
+
+        attributes
+      end
+
+      def insert(attributes)
+        attributes = Util.deep_dup(attributes)
+
+        record_id = record_id_for(attributes)
+
+        if record_id
+          # TODO: implement exists? using redis instead of super's find
+          if exists?(record_id)
+            raise CannotInsertError.new(record_id, "already exists")
+          end
+        else
+          record_id = get_id
+          attributes.merge!(primary_key_attribute => record_id)
+        end
+
+        prepare_attributes_for_redis(attributes)
+
+        # TODO: use redis.multi here
+        redis.hset(record_key_prefix(record_id), attributes)
+        redis.zadd(primary_keys_index_key, record_id, record_id)
+        find(record_id)
+      end
+
+      def update(attributes)
+        record_id = record_id_for(attributes)
+
+        unless exists?(record_id)
+          # :nocov:
+          raise CannotUpdateError.new(record_id, "does not exist")
+          # :nocov:
+        end
+
+        attributes = prepare_attributes_for_redis(Util.deep_dup(attributes))
+
+        redis.hset(record_key_prefix(record_id), attributes)
+        find(record_id)
+      end
+
+      def hard_delete(record_id)
+        # TODO: add multi
+        key = record_key_prefix(record_id)
+
+        if redis.del(key) != 1
+          # :nocov:
+          raise CannotUpdateError.new(record_id, "#{key} does not exist")
+          # :nocov:
+        end
+
+        unless redis.zrem(primary_keys_index_key, record_id)
+          # :nocov:
+          raise "Unexpected: when deleting #{key}, " \
+                "#{record_id} was not present in the primary key index for #{primary_keys_index_key}"
+          # :nocov:
+        end
+      end
+
+      def hard_delete_all
+        batches_of_primary_keys.each do |batch|
+          redis.pipelined do |p|
+            batch.each do |record_id|
+              p.del(record_key_prefix(record_id))
+              p.zrem(primary_keys_index_key, record_id)
+            end
+          end
+        end
+      end
+
+      private
+
+      def redis
+        raw_connection
+      end
+
+      def prepare_attributes_for_redis(attributes)
+        json_serialized_attributes.each do |attribute_name|
+          if attributes.key?(attribute_name)
+            attributes[attribute_name] = JSON.fast_generate(attributes[attribute_name])
+          end
+        end
+
+        attributes
+      end
+
+      def restore_attributes_from_redis(attributes)
+        attributes.transform_keys!(&:to_sym)
+
+        json_serialized_attributes.each do |attribute_name|
+          if attributes.key?(attribute_name)
+            attributes[attribute_name] = JSON.parse(attributes[attribute_name])
+          end
+        end
+
+        attributes
+      end
+
+      def json_serialized_attributes
+        @json_serialized_attributes ||= begin
+          attribute_names = []
+
+          entity_class.attributes_type.element_types.each_pair do |attribute_name, attribute_type|
+            if attribute_type.extends_symbol?(:duckture)
+              attribute_names << attribute_name
+            end
+          end
+
+          attribute_names
+        end
+      end
+
+      def batches_of_primary_keys
+        limit = [0, 50]
+
+        lower_bound = 0
+
+        Enumerator.new do |yielder|
+          loop do
+            batch = redis.zrangebyscore(primary_keys_index_key, lower_bound, "+inf", limit:)
+
+            break if batch.empty?
+
+            yielder << batch
+
+            lower_bound = batch.last.to_i + 1
+          end
+        end.lazy
+      end
+    end
+  end
+end
